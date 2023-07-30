4cd890e6835621042b65b3faa72aa93460c4fb4c
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-30 13:51:08 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@
 // example: const header = Header.header()
 import Header from "./components/Header";
 
+// how to use a class instead of using a function:
+/* ------------------
+first you need to import React:
+
+  import React from "react";
+
+then using the React library, you can declare the class,
+but then you want to add more methods and functions
+(like the lifecycle methods) by using the keyword "extends"
+
+  class App extends React.Component {
+    // render() takes care of the output
+    render() {
+      // return whatever JSX you want
+      return(
+        <h1>
+          some message
+        </h1>
+      )
+    }
+  }
+   ------------------ */
+
 function App() {
 
-  // the <div className="App"> can be any one html element,
+  // the <div className="container"> can be any one html element,
   // it could be a fragment (<> </>) or a <div> like it is
   return (
-    <div className="App">
-      <Header />
+    <div className="container">
+      <Header title="Task Tracker" />
     </div>
   );
 }
