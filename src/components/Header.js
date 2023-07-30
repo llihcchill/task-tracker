4a31f293a4ad05af598514751f22be478a0cf5bb
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-30 13:51:31 +1000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 // variable into a function (arrow function)
-const Header = () => {
+const Header = ({title}) => {
+  // you can input props into the function part of an arrow function
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
+
+// how to set default props:
+/* ------------------------
+the {component}.defaultProps function can be used:
+
+Header.defaultProps = {
+  title: "Task Tracker";
+}
+   ------------------------ */
 
 export default Header;
