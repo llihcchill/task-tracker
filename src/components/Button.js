9b1700e735013513413023ae3d9e3d70da6cc096
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-31 18:16:06 +1000

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <button className="btn">Add</button>
+import PropTypes from "prop-types";
+
+const Button = ({ colour, text }) => {
+  // make sure with props you don't put two entire ones in a row like this:
+  // { colour }, { text }
+  return <button className="btn" style={{ backgroundColor: colour }}>{text}</button>
+  // make sure with the style as well you don't just put in the prop variable and not the
+  // CSS attribute you want to actually change
+}
+
+Button.defaultProps = {
+  text: "Add",
+  colour: "green",
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  colour: PropTypes.string,
 }
 
 export default Button;
