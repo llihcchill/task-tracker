fdd97d92315c38841f71e20e82b2f87642f3eebf
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-08-02 20:51:33 +1000

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,8 @@
 import { FaTimes } from "react-icons/fa"
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div className={`task ${task.reminder ? "reminder" : ""}`} onDoubleClick={() => onToggle(task.id)}>
       <h3>{task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} onClick={() => onDelete(task.id)} /></h3>
       <p>{task.day}</p>
     </div>
@@ -12,6 +12,10 @@ const Task = ({ task, onDelete }) => {
 
   // also not to access props in the arrow function, you don't have to put extra curly brackets
   // around the prop
+
+  // the div className= is essentially if the reminder attribute of the task (reminder: true) is true,
+  // then it will change the class to "reminder" which adds a little green drape over the task, and if not,
+  // it will return no class onto the div
 }
 
 export default Task
